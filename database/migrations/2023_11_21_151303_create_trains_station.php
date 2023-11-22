e9bca7ce8690647e0e37580f4c55a07017ec8d9f
Melozzo <carmecamma1995@gmail.com>
2023-11-22 18:37:52 +0100

    [edit]: train station seeder

--- database/migrations/2023_11_21_151303_create_trains_station.php
+++ database/migrations/2023_11_21_151303_create_trains_station.php
@@ -19,9 +19,9 @@ return new class extends Migration
             $table->string('agency');
             $table->string('departure_station');
             $table->string('arrival_station');
-            $table->dateTime('departure_date', $precision = 0);
-            $table->dateTime('arrival_date', $precision = 0);
-            $table->char('code_train');
+            $table->dateTime('departure_date');
+            $table->dateTime('arrival_date');
+            $table->char('code_train',8);
             $table->tinyInteger('carriage_number');
             $table->boolean('train_on_schedule');
             $table->boolean('train_cancelled');
